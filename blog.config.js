9eed4f7e120a9499b4dbd2ee8a3bc84023b12aa9
Deepak Mishra <trtl@Deepaks-MacBook-Pro.local>
2022-06-09 12:11:28 +0530

    basic changes

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'skilp4d',
+  author: 'Deepak Mishra',
+  email: 'skilp4d.dm@gmail.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: "Deepak Mishra's machine learning and tech blog",
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
